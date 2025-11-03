7822c4de437a9ad08947bf95b6895aeb7aa9e98d
FernandoHos <hoseafernando5072@gmail.com>
2025-11-03 08:23:38 +0700

    Update notification_service.dart

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,5 +1,3 @@
-// 📁 lib/services/notification_service.dart
-
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
